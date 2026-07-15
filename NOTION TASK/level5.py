bc946255d0eda3e07fa9e13d3a8db899cf624c46
harshkoriya6-ctrl <harshkoriya6@gmail.com>
2026-07-15 23:25:58 +0530

    level-5

---
+++ NOTION TASK/level5.py
@@ -0,0 +1,25 @@
+# for i in range (1,11):
+#     print(i)
+
+# for i in range (10,0,-1):
+#     print(i)
+
+# n=int(input("Enter Number: "))
+# for i in range (1,11):
+#     print(n, "*", i, "=", n*i)
+
+# num=int(input("Enter Number:"))
+# sum=0
+# for i in range (1,num+1):
+#     sum=sum+i
+#     print("Sum is:",sum)
+
+
+
+# num=int(input("Enter Number:"))
+# count=0
+# while num > 0:
+#     num=num//10
+#     count=count+1
+#     print(count)
+
